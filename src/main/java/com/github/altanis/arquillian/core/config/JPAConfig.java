929a030ad61a11e3b18bf228f913ce71257373d2
Sebastian Laskawiec <sebastian.laskawiec@payu.pl>
2014-06-07 17:25:19 +0200

    @Produces

--- src/main/java/com/github/altanis/arquillian/core/config/JPAConfig.java
+++ src/main/java/com/github/altanis/arquillian/core/config/JPAConfig.java
@@ -1,13 +1,24 @@
 package com.github.altanis.arquillian.core.config;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import com.github.altanis.arquillian.core.cdi.ProperItemsRepository;
+import com.github.altanis.arquillian.core.items.ItemsRepository;
+import com.github.altanis.arquillian.core.items.impl.JPAItemsRepository;
 
 public class JPAConfig {
 
     @Produces
     @PersistenceContext(unitName = "default-pu")
     EntityManager entityManager;
+
+    @Produces
+    @ApplicationScoped
+    @ProperItemsRepository
+    public ItemsRepository createItemsRepository() {
+        return new JPAItemsRepository("JPARepo", entityManager);
+    }
 
 }
--- src/main/java/com/github/altanis/arquillian/core/items/impl/JPAItemsRepository.java
+++ src/main/java/com/github/altanis/arquillian/core/items/impl/JPAItemsRepository.java
@@ -1,19 +1,20 @@
 package com.github.altanis.arquillian.core.items.impl;
 
 import java.util.Collection;
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import com.github.altanis.arquillian.core.cdi.ProperItemsRepository;
 import com.github.altanis.arquillian.core.items.Item;
 import com.github.altanis.arquillian.core.items.ItemsRepository;
 
-@ApplicationScoped
-@ProperItemsRepository
 public class JPAItemsRepository implements ItemsRepository {
 
-    @Inject
     private EntityManager em;
+
+    private String repositoryName;
+
+    public JPAItemsRepository(String repositoryName, EntityManager em) {
+        this.repositoryName = repositoryName;
+        this.em = em;
+    }
 
     @Override
     public void addItem(Item item) {
